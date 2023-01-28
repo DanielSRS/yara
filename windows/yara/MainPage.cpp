75063b5554f5c0ff87b9fbd91dcf773e0ea779b3
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-01-28 14:32:07 -0300

    ✨ Extend view to titleBar

--- windows/yara/MainPage.cpp
+++ windows/yara/MainPage.cpp
@@ -4,16 +4,28 @@
 #include "MainPage.g.cpp"
 #endif
 
+#include <winrt/Windows.UI.ViewManagement.h> 
+#include <winrt/Windows.ApplicationModel.Core.h>  
+#include <winrt/Windows.UI.h>  
+
 #include "App.h"
 
 using namespace winrt;
 using namespace xaml;
+
+using namespace winrt::Windows::UI;
+using namespace winrt::Windows::UI::ViewManagement;
+using namespace winrt::Windows::ApplicationModel::Core;
 
 namespace winrt::yara::implementation
 {
     MainPage::MainPage()
     {
         InitializeComponent();
+
+        auto currentView = CoreApplication::GetCurrentView();
+        auto currentTitleBar = currentView.TitleBar();
+        currentTitleBar.ExtendViewIntoTitleBar(true);
         auto app = Application::Current().as<App>();
         ReactRootView().ReactNativeHost(app->Host());
     }
