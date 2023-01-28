17e9dbc41b01d0e310b55e15eeb1d01ace0025c2
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-01-28 13:18:33 -0300

    ✨ Add windows project

---
+++ windows/yara/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::yara::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::yara::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
