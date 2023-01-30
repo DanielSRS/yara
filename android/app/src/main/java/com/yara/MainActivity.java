f86e414a90269179bae607373887c5d93bf41b03
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-01-29 21:16:36 -0300

    ➕ Add react navigation
    Configure react-native-screens on android;

--- android/app/src/main/java/com/yara/MainActivity.java
+++ android/app/src/main/java/com/yara/MainActivity.java
@@ -5,6 +5,9 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+/** Required configuration for react-native-screens (native lib) to work. */
+import android.os.Bundle;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -31,5 +34,13 @@ public class MainActivity extends ReactActivity {
         // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
         DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
         );
+  }
+
+  /**
+   * Required configuration for react-native-screens (native lib) to work.
+   */
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
