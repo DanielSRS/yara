915b155f2531d6badba08218abf06a861eb563fb
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-02-22 10:02:16 -0300

    🔧 Update Android project name

--- android/app/src/main/java/com/iara/MainActivity.java
+++ android/app/src/main/java/com/iara/MainActivity.java
@@ -1,4 +1,4 @@
-package com.yara;
+package com.iara;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -16,7 +16,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "yara";
+    return "Iara";
   }
 
   /**
--- android/app/src/main/java/com/iara/MainApplication.java
+++ android/app/src/main/java/com/iara/MainApplication.java
@@ -1,4 +1,4 @@
-package com.yara;
+package com.iara;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
