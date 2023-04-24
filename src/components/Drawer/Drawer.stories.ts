92361f54011bd898bbef324663814e64fc5ccad1
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-04-23 22:02:32 -0300

    ✨ Add Drawer storie

---
+++ src/components/Drawer/Drawer.stories.ts
@@ -0,0 +1,10 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react-native';
+import React from 'react';
+import { Drawer } from './Drawer';
+
+export default {
+  title: 'components/Drawer',
+  component: Drawer,
+} as ComponentMeta<typeof Drawer>;
+
+export const Basic: ComponentMeta<typeof Drawer> = {};
