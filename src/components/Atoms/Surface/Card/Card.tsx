0266c7ec5d18c75d360275e544faac0b13415c1b
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2023-04-28 09:04:34 -0300

    💄 Update SurfaceCard to use useColorScheme

--- src/components/Atoms/Surface/Card/Card.tsx
+++ src/components/Atoms/Surface/Card/Card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleProp, StyleSheet, View, ViewStyle } from "react-native";
+import { StyleProp, StyleSheet, View, ViewStyle, useColorScheme } from "react-native";
 
 type SurfaceCardProps = {
   children?: React.ReactNode;
@@ -8,8 +8,10 @@ type SurfaceCardProps = {
 
 export function SurfaceCard(props: SurfaceCardProps) {
   const { style } = props;
+  const isDarkTheme = useColorScheme() === 'dark';
+  const defaultStyles = isDarkTheme ? styles.dark : styles.light;
   return (
-    <View {...props} style={[styles.defaultStyles, style]}>
+    <View {...props} style={[defaultStyles, style]}>
       {}
       {}
     </View>
@@ -17,15 +19,22 @@ export function SurfaceCard(props: SurfaceCardProps) {
 }
 
 const styles = StyleSheet.create({
-  defaultStyles: {
+  light: {
     /** Strock */
     borderWidth: 1,
-    // borderColor: 'rgba(0, 0, 0, 0.0578)', // light
+    borderColor: 'rgba(0, 0, 0, 0.0578)', // light
+    borderRadius: 7,
+
+    /** Fill */
+    backgroundColor:  'rgba(255, 255, 255, 0.7)', // light
+  },
+  dark: {
+    /** Strock */
+    borderWidth: 1,
     borderColor: 'rgba(0, 0, 0, 0.1)', // dark
     borderRadius: 7,
 
     /** Fill */
-    // backgroundColor:  'rgba(255, 255, 255, 0.7)', // light
     backgroundColor:  'rgba(255, 255, 255, 0.0512)', // dark
   },
 });
