9b292762a6e30bd31fb95706f370e39545f18468
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2024-01-20 10:32:36 -0300

    🚚  Move size conversions to utils

--- src/components/File/File.tsx
+++ src/components/File/File.tsx
@@ -9,16 +9,7 @@ import {
 import { DefaultText } from '../Text';
 import { SurfaceCard } from '../Atoms';
 import { FluentIcon } from '../../Libs/Icons/Fluent/FluentIcons';
-
-const sizes = {
-  B: 1,
-  KB: 1000,
-  MB: 1000000,
-  GB: 1.0e9,
-  TB: 1.0e12,
-  PB: 1.0e15,
-  '??': 1,
-} as const;
+import { convertByteUnitys } from '../../utils/size/sizeConverters';
 
 interface FileProps {
   /** Nome do arquivo */
@@ -34,51 +25,6 @@ interface FileProps {
   /** Função de callback ao pressionar o arquivo */
   onPress?: () => void;
 }
-
-/**
- * f :: number -> string
- *
- * Recebe o valor absoluto em bytes e retorna a
- * unidade de medida em bytes na escala mais apropriada
- */
-const getSizeUnityScale = (absoluteValue: number) => {
-  if (absoluteValue < sizes.KB) {
-    return 'B';
-  }
-  if (absoluteValue < sizes.MB) {
-    return 'KB';
-  }
-  if (absoluteValue < sizes.GB) {
-    return 'MB';
-  }
-  if (absoluteValue < sizes.TB) {
-    return 'GB';
-  }
-  if (absoluteValue < sizes.PB) {
-    return 'TB';
-  }
-
-  return '??';
-};
-
-type ConversionUnitys = keyof typeof sizes;
-const changeUnityScale = (targetScale: ConversionUnitys) => (value: number) =>
-  value / sizes[targetScale];
-const convertSizeUnity =
-  // uma função que recebe o valor e retorna a unidade
-
-
-    (b: (a: number) => ConversionUnitys) =>
-    // recebe uma função para ajustar a escala
-    (value: (v: ConversionUnitys) => (a: number) => number) =>
-    // o valor absoluto a ser convertido
-    (absoluteValue: number) => {
-      const unity = b(absoluteValue);
-      return `${value(unity)(absoluteValue)
-        .toFixed(2)
-        .replace('.', ',')} ${unity}`;
-    };
-const convertByteUnitys = convertSizeUnity(getSizeUnityScale)(changeUnityScale);
 
 /**
  * Exibe as informações de um arquivo de um torrent
