0d98781c8be6d47f99d4f126431466fc0d0c0549
Daniel Santa Rosa <Daniel_srs_@hotmail.com>
2024-01-20 07:06:05 -0300

    ✨ Add more fluent icons

--- src/Libs/Icons/Fluent/FluentIcons.tsx
+++ src/Libs/Icons/Fluent/FluentIcons.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { SvgXml } from 'react-native-svg';
+import { NumberProp, SvgXml } from 'react-native-svg';
 import { FluentIconNames, icons } from './IconstSvg';
 
 interface FluentIconProps {
   color: string | undefined;
   name: FluentIconNames;
-  width: number | string | undefined;
-  height: number | string | undefined;
+  width: NumberProp | undefined;
+  height: NumberProp | undefined;
 }
 
 export const FluentIcon = (props: FluentIconProps) => {
-  const { color = '#FFFFFF', name, height = '100%', width = '100%' } = props;
+  const { color = '#FFFFFF', name, height, width } = props;
   const xml = icons[name](color);
   return <SvgXml width={width} height={height} xml={xml} />;
 };
